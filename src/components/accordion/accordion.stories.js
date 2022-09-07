4f5b14c7c5592618382547858c1c58929a7fb756
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-09-07 18:54:07 +0200

    patch accordion

--- src/components/accordion/accordion.stories.js
+++ src/components/accordion/accordion.stories.js
@@ -2,9 +2,7 @@ import Accordion from './accordion.svelte';
 
 export default {
     title: 'Components/Accordion',
-    component: Accordion,
-    faqTile: {control: 'text'},
-    faqContent: {control: 'text'},
+    component: Accordion
 };
 
 const Template = (args) => ({
@@ -17,14 +15,10 @@ const Template = (args) => ({
 
 export const Title = Template.bind({});
 Title.args = {
-    idInput: "Title",
-    labelInput: 'Title',
-    typeInput: 'text'
-};
-
-export const Content = Template.bind({});
-Content.args = {
-    idInput: "Content",
-    labelInput: 'Content',
-    typeInput: 'text'
+    faqList :  [
+        {title : "How to pay ?", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "},
+        {title : "Why us ?", content: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."},
+        {title : "20 or nothing?", content: "Yes."},
+        {title : "Who some we ?", content: "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."},
+    ]
 };
