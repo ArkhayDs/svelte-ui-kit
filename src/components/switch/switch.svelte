ae859e1a5e2701b98a0647033cbb24c8d778bcfa
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-09-07 14:08:50 +0200

    switch component

---
+++ src/components/switch/switch.svelte
@@ -0,0 +1,22 @@
+<script>
+   function checked(node) {
+	
+	function handleChange() {
+		if (node.checked) {
+			node.parentNode.parentNode.classList.add('dark-mode')
+			console.log("Checked");
+		} else {
+			node.parentNode.parentNode.classList.remove('dark-mode')
+			console.log("Not checked");
+		}
+	}
+	
+	node.addEventListener('change', handleChange);
+	
+	return {
+		destroy() {
+			node.removeEventListener('change', handleChange);
+		}
+	}
+} 
+</script>
